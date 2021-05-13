95d038629149e87a4fb299161fc295565762af68
Phil <pmulholland@shincbm.com>
2021-05-13 23:49:36 +0930

    Update to C++17 and C++20 split.

--- examples/svd_cxx17_headers.sh
+++ examples/svd_cxx17_headers.sh
@@ -1,16 +1,17 @@
 # Convert an SVD example to HTML
-if [ ! -d device_cxx_headers/ ] ; then
-    mkdir device_cxx_headers/
+VER=cxx17
+if [ ! -d device_${VER}_headers/ ] ; then
+    mkdir device_${VER}_headers/
 fi
 SVD_IN=../extern/cmsis-svd/data/ARM_SAMPLE/CMSDK_CM3.svd
 ../generators/svd_jinja.py \
-    --out-path device_cxx_headers/ \
+    --out-path device_${VER}_headers/ \
     --device device.hpp \
     --device test_device.cpp \
     --peripheral peripheral_mmio_param.hpp \
     --peripheral peripheral_mmio_regs.hpp \
     --peripheral peripheral_mmio_dev.hpp \
-    --templates ../cxx_mmio_interface/template/ \
+    --templates ../${VER}_mmio_interface/template/ \
     ${SVD_IN}
 
-make -C device_cxx_headers/
+make -C device_${VER}_headers/
